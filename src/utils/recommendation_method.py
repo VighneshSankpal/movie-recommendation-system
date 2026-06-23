092cb32b2bc33e8faf3ce594e91fb677c1b94829
vighnesh <vighneshsankpal99@gmail.com>
2026-06-23 22:38:42 +0000

    both model are implimented. in python file write using functions

--- src/utils/recommendation_method.py
+++ src/utils/recommendation_method.py
@@ -104,14 +104,100 @@ def get_similarity_explaination(movie_idx,remm_idx):
 
 
 
-if __name__ == '__main__':
-    movie_idx = 76
-    recommended_movies_info =get_movies_content_based(movie_idx)
 
-    for rmc in recommended_movies_info:
-        print(f'Movie Title is : {rmc.movie_title}')
-        print(f'Movie matching_keywords is : {rmc.matching_keywords}')
-        print(f'Movie matching_gernes is : {rmc.matching_gernes}')
-        print(f'Movie overview_similarity is : {rmc.overview_similarity}')
-        print(f'Movie matching_director is : {rmc.matching_director}')
-        print('-'*25)
+
+#  COllaborative filtering Area::
+
+import json
+import pickle
+
+# Import trained SVD model.
+with open('artifact/svd_model.pkl','rb') as file:
+    svd_model = pickle.load(file)
+
+# user watched movies data
+with open('dataset/processed/userWatched_movies.json','r') as file:
+    userWatched_movies = json.load(file)
+userWatched_movies = pd.Series(userWatched_movies)
+
+
+
+def collaborative_based_filtering(user_id):
+    '''
+    apply collaborative based filtering on given movie index and return the top similarities movies.
+    input:
+    user_id : moviesLens rating user_id which we want to recommand movies.
+
+    return :
+    top similarity movies list.
+    ''' 
+
+     # find the index of movielens movie based on index value
+    # get all movies which exist in dataset.
+    all_movies = set(movieId_lookup['movieLens_movieId'].tolist())
+    
+    movies_watched = set(userWatched_movies[str(user_id)])
+    
+    
+    if not type(movies_watched) == set:
+        movie_watched = set(movie_watched)
+        
+    unwatched_movies = all_movies - movies_watched
+
+    
+    predictions = []
+    # calculate the prediction rating for user U for unwatched movie uM
+    for movie in unwatched_movies:
+        pred_rating = svd_model.predict(user_id,movie).est
+        predictions.append([movie,pred_rating])
+       
+    
+    # sort the predicted rating by highly rated movie prediction
+    predictions.sort(key=lambda x : x[1],reverse=True)
+
+
+    
+    movie_lookup = dict(zip(movieId_lookup['movieLens_movieId'],movieId_lookup['title_clean']))
+    
+    top_predictions = []
+    for movieId, rating in predictions[:12]:
+        #conver prediction movies into diction object
+        rm_dict =  {
+            "title": movie_lookup[movieId],
+            "predicted_rating": np.round(rating,2),
+            'movieId' :movieId
+
+            }
+
+        # append rm object into list
+        top_predictions.append(rm_dict)
+
+    return top_predictions    
+
+    
+
+
+#  Test Content Based filters
+
+# if __name__ == '__main__':
+#     movie_idx = 76
+#     recommended_movies_info =get_movies_content_based(movie_idx)
+
+#     for rmc in recommended_movies_info:
+#         print(f'Movie Title is : {rmc.movie_title}')
+#         print(f'Movie matching_keywords is : {rmc.matching_keywords}')
+#         print(f'Movie matching_gernes is : {rmc.matching_gernes}')
+#         print(f'Movie overview_similarity is : {rmc.overview_similarity}')
+#         print(f'Movie matching_director is : {rmc.matching_director}')
+#         print('-'*25)
+
+
+
+
+#  Test Collaborative Based filters  
+
+if __name__ == '__main__':
+    print('Collaborative Filtering result : ')
+    result = collaborative_based_filtering(user_id=4019)
+    for rm in result:
+        print(f'Title : {rm['title']}, rating : {rm['predicted_rating']}, movieId is {rm['movieId']}')
