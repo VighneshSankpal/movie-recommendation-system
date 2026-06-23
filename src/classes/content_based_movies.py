af66761c1e9365faeccf9f4ef3327d4dee9c058f
vighnesh <vighneshsankpal99@gmail.com>
2026-06-23 19:33:48 +0000

    created  method which return the list of recomendation movie objects

---
+++ src/classes/content_based_movies.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import numpy as np
+
+
+tmdb_movies_info = pd.read_csv('dataset/processed/tmdb_movies_info.csv')
+
+
+
+class ContentRecommendationMovie:
+    '''
+     Save information and find similaraties and explaination of recommendated movie
+    '''
+    def __init__(self,movie_idx,overview_similarity):
+        '''
+            Input : Index of recommendated movie object ( tmdb table index )
+        '''
+        self.movie_idx = movie_idx
+        
+        self.movie_title = None
+        self.matching_keywords = None
+        self.matching_gernes = None
+        self.matching_director = None
+        self.overview_similarity = round(overview_similarity,2)
+   
+    def calcualte_similarities(self,recommended_tmdb_idx):
+        '''
+            Calcluate the similarities between current movies information and given movies.
+            and store it data into data members.
+        '''
+
+        cm = tmdb_movies_info.loc[self.movie_idx]
+        rm = tmdb_movies_info.loc[recommended_tmdb_idx]
+       
+        
+        # keywords 
+        cm_keys = set(cm['keywords_clean'].strip().split(', '))
+        rm_keys = set(rm['keywords_clean'].strip().split(', '))
+        
+        matching_keywords = list(rm_keys & cm_keys )
+        matching_keywords.sort()
+        
+        # genres
+        cm_genres = set(cm['genres_clean'].strip().split(', '))
+        rm_genres = set(rm['genres_clean'].strip().split(', '))
+    
+        matching_gernes = list(cm_genres & rm_genres)
+        matching_gernes.sort()
+    
+        # director matching
+        cm_director = cm['crew_clean'].strip().split('Director: ')[1]
+        rm_director = rm['crew_clean'].strip().split('Director: ')[1]
+    
+        if cm_director == rm_director:
+            matching_director = True
+        else:
+            matching_director = False
+    
+        
+        # Movie Overview
+
+        
+        self.movie_title = cm['title']
+        self.matching_keywords = matching_keywords
+        self.matching_gernes   = matching_gernes
+        self.matching_director = matching_director
+
+        return {
+             'title'             : self.movie_title,
+             'matching_keywords' :  self.matching_keywords,
+             'matching_gernes'   : self.matching_gernes,
+             'matching_director' : self.matching_director,
+             'overview_similarity' : self.overview_similarity
+        }
+    
+
+if __name__ == '__main__':
+    current_movie_idx = 0
+    match_score = content_based_filtering(current_movie_idx) # movie index of current movie
+
+    recommended_movies_info = []
+    for recommended_movie in match_score:
+        rm = ContentRecommendationMovie(recommended_movie['index'],recommended_movie['overview_similarity'])
+        rm.calcualte_similarities(current_movie_idx)
+
+        recommended_movies_info.append(rm)
+
+    for rmc in recommended_movies_info:
+        print(f'Movie Title is : {rmc.movie_title}')
+        print(f'Movie matching_keywords is : {rmc.matching_keywords}')
+        print(f'Movie matching_gernes is : {rmc.matching_gernes}')
+        print(f'Movie overview_similarity is : {rmc.overview_similarity}')
+        print(f'Movie matching_director is : {rmc.matching_director}')
+        print('-'*25)
+
+        
---
+++ src/utils/recommendation_method.py
@@ -0,0 +1,117 @@
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+import numpy as np
+
+tmdb_movies_info = pd.read_csv('dataset/processed/tmdb_movies_info.csv')
+movieId_lookup = pd.read_csv('dataset/processed/movieId_lookup.csv')
+
+combined_embeddings = np.load('artifact/embeddings/final_embeddings.npy')
+director_embeddings = np.load('artifact/embeddings/director_embeddings.npy')
+keyword_embeddings = np.load('artifact/embeddings/keyword_embeddings.npy')
+genre_embeddings = np.load('artifact/embeddings/genre_embeddings.npy')
+overview_embeddings = np.load('artifact/embeddings/overview_embeddings.npy')
+
+
+
+from src.classes.content_based_movies import ContentRecommendationMovie
+
+def get_movies_content_based(movie_idx):
+
+    '''
+    Return the top movies of it.
+    '''
+     
+    match_score = content_based_filtering(movie_idx) # movie index of current movie
+
+    recommended_movies_info = []
+    for recommended_movie in match_score:
+        rm = ContentRecommendationMovie(recommended_movie['index'],recommended_movie['overview_similarity'])
+        rm.calcualte_similarities(movie_idx)
+
+        recommended_movies_info.append(rm)
+    
+    return recommended_movies_info
+
+
+
+# get movies intedex 
+
+def content_based_filtering(movie_idx):
+    '''
+    apply content based filtering on given movie index and return the top similarities movies.
+    input:
+    movie_idx : index value of movieId_lookup table.
+
+    return :
+    top similarity movies list.
+    '''
+   
+    # find the index of tmdb movie based on index value
+    tmdb_movieId = movieId_lookup.iloc[movie_idx]['tmdb_movieId']
+    
+    # calculate movie similarity based on embedded text vector metrix.
+    tmdb_index = tmdb_movies_info[tmdb_movies_info['movie_id'] == tmdb_movieId].index[0]
+    similarities = cosine_similarity([combined_embeddings[tmdb_index]],combined_embeddings)[0]
+
+    # sort index based on values then change the order into descending and give first 10 values index
+    top_indicies = similarities.argsort()[::-1][:11]
+
+    # # calculate score of movide matching
+    top_similar_movies  = []
+    for  rm in top_indicies[1:]:
+        
+        movie_info = {'movie_title' : tmdb_movies_info.loc[rm,'title'],'index' :rm}
+        
+        movie_similarity = get_similarity_explaination(tmdb_index,rm)
+        
+        movie_info.update(movie_similarity)
+        
+        top_similar_movies.append(movie_info)
+
+        
+
+    return top_similar_movies
+    
+    # for similary_tmdb_id in top_indicies:
+    # for movie_title in  tmdb_movies_info.loc[top_indicies,'title']:
+    #     print(movie_title)
+    # return tmdb_movies_info.loc[top_indicies,'title'],match_score
+    
+    
+   
+
+
+
+
+
+def get_similarity_explaination(movie_idx,remm_idx):
+    gen_sim = cosine_similarity(genre_embeddings[movie_idx].reshape(1,-1),genre_embeddings[remm_idx].reshape(1,-1))[0,0]
+
+    overview_sim = cosine_similarity(overview_embeddings[movie_idx].reshape(1,-1),overview_embeddings[remm_idx].reshape(1,-1))[0,0]
+
+    keyword_sim = cosine_similarity(keyword_embeddings[movie_idx].reshape(1,-1),keyword_embeddings[remm_idx].reshape(1,-1))[0,0]
+
+    final_score =(gen_sim*30 + overview_sim*40 + keyword_sim  *30 )
+    
+
+    return {
+            'genre_similarity' : np.round(gen_sim, 2),
+            "keyword_similarity": np.round(keyword_sim,2),
+            "overview_similarity": np.round((overview_sim*100),2),      
+            "final_score": np.round(final_score,2)
+            }
+
+
+
+
+if __name__ == '__main__':
+    movie_idx = 76
+    recommended_movies_info =get_movies_content_based(movie_idx)
+
+    for rmc in recommended_movies_info:
+        print(f'Movie Title is : {rmc.movie_title}')
+        print(f'Movie matching_keywords is : {rmc.matching_keywords}')
+        print(f'Movie matching_gernes is : {rmc.matching_gernes}')
+        print(f'Movie overview_similarity is : {rmc.overview_similarity}')
+        print(f'Movie matching_director is : {rmc.matching_director}')
+        print('-'*25)
